7c528cf9d03ad691590500d4f13124f29a437e50
heyrict <xiezh0831@126.com>
2019-01-16 20:12:06 +0800

    Add multiple columns

--- extract.py
+++ extract.py
@@ -4,12 +4,29 @@ import os
 import pandas as pd
 
 
-def extract_data(filename, sheet=0, key="学号", value="期末成绩"):
+def extract_data(filename,
+                 sheet=0,
+                 key="学号",
+                 ps="平时成绩",
+                 qz="期中成绩",
+                 qm="期末成绩",
+                 zp="总评成绩"):
+    def extract_record(record):
+        returns = {}
+        if ps is not None and ps in record:
+            returns["ps"] = record[ps]
+        if qz is not None and qz in record:
+            returns["qz"] = record[qz]
+        if qm is not None and qm in record:
+            returns["qm"] = record[qm]
+        if zp is not None and zp in record:
+            returns["zp"] = record[zp]
+        return [record[key], returns]
+
     df = pd.read_excel(filename, sheet_name=sheet)
-    export_list = list(df.apply(lambda x: [x[key], x[value]], axis=1))
+    export_list = list(df.apply(extract_record, axis=1))
     export_dict = dict(export_list)
-    with open("%s_%s.txt" % (filename, sheet), "w") as f:
-        f.write(str(export_list))
+    return str(export_list)
 
 
 def get_prompt_msg(prompt, default=None):
@@ -41,5 +58,12 @@ if __name__ == "__main__":
     filename = get_input("请输入文件名", validate_file)
     sheet = get_input("请输入工作簿序号 ( 从 0 开始计数 )", default=0, process=int)
     key = get_input("请输入学号列的列名", default="学号")
-    value = get_input("请输入期末成绩的列名", default="期末成绩")
-    extract_data(filename, sheet, key, value)
+    ps = get_input("请输入平时成绩的列名", default=None)
+    qz = get_input("请输入期中成绩的列名", default=None)
+    qm = get_input("请输入期末成绩的列名", default=None)
+    zp = get_input("请输入总评成绩的列名", default=None)
+    data = extract_data(filename, sheet, key, value)
+    with open("paste_me_in_browser_console.js") as f:
+        script = f.read()
+    with open("%s_%s" % (filename, sheet), "w") as f:
+        f.write(script[:7] + data + script[9:])
--- paste_me_in_browser_console.js
+++ paste_me_in_browser_console.js
@@ -1,11 +1,10 @@
-/*
- * 使用方法：
- * 1. 将 data = {} 中的 `{}` 替换为 extract.py 生成的文件内容
- * 2. 打开浏览器控制台（通常按 F12 快捷键）
- * 3. 将该文件复制粘贴至脚本最下方输入栏，回车。
-*/
-
 data = {};
+
+function fillcol(tr, col, value) {
+  if (value !== undefined) {
+    tr.children[col].children[0].value = score;
+  }
+}
 
 var tbody = document
   .getElementById("frame_content")
@@ -15,7 +14,9 @@ for (i in tbody.children) {
   var tr = tbody.children[i];
   if (tr.children === undefined) continue;
   var studentid = tr.children[1].innerText;
-  var score = data[parseInt(studentid)]; /* 假定导出表格学号栏数据格式为整数 */
-  if (score === undefined) continue; /* 学号不匹配，不填充 */
-  tr.children[5].children[0].value = score; /* 将成绩填充表格第 5 栏（期末成绩） */
+  var scores = data[parseInt(studentid)]; /* 假定导出表格学号栏数据格式为整数 */
+  fillcol(tr, 2, score.ps);
+  fillcol(tr, 3, score.qz);
+  fillcol(tr, 4, score.qm);
+  fillcol(tr, 5, score.zp);
 }
